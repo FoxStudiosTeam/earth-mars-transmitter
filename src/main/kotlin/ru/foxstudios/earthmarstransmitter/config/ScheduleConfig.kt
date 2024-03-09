7d5fbda4d4f50b710b8adb1d8f6c8a75db6e5cc0
AgniaEndie <Endienasg3@gmail.com>
2024-03-09 23:25:31 +0500

    fix udp packet

--- src/main/kotlin/ru/foxstudios/earthmarstransmitter/config/ScheduleConfig.kt
+++ src/main/kotlin/ru/foxstudios/earthmarstransmitter/config/ScheduleConfig.kt
@@ -1,13 +1,15 @@
 package ru.foxstudios.earthmarstransmitter.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
+import reactor.core.publisher.Mono
 import reactor.netty.udp.UdpClient
 import ru.foxstudios.earthmarstransmitter.repositories.IDataRepository
-
 @Configuration
 @EnableScheduling
 class ScheduleConfig(@Autowired var repository : IDataRepository) {
@@ -15,9 +17,11 @@ class ScheduleConfig(@Autowired var repository : IDataRepository) {
     fun sendDataScheduled(){
         val logger = LoggerFactory.getLogger(this.javaClass)
         val list = repository.findAllBySended(false)
+        val mapper = ObjectMapper().registerKotlinModule()
         for (elem in list){
+            val text = mapper.writeValueAsString(elem)
             val client = UdpClient.create().port(25578).host("host.docker.internal").wiretap(true).connectNow()
-            client.outbound().sendObject(elem).then().subscribe()
+            client.outbound().sendString(Mono.just(text)).then().subscribe()
             client.inbound().receive().asString().doOnNext{next ->
                 if(next == "ok"){
                     logger.info("send")
